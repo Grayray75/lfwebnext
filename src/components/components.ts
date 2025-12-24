c223c55a944da7d252d98efcfd2d331e3e2e50a1
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2025-12-24 12:59:29 +0100

    Embed meta directly into head

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,5 +1,4 @@
 export { Usage } from './usage';
-export { LfMeta } from './head-meta';
 export { ModListElement } from './mod-list';
 export { NavBarElement } from './navbar';
 export { ShowcaseGallery } from './showcase-gallery';
